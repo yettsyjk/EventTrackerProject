797a9a307cc34571987aeb7d2597b63bf851b741
Yettsy Knapp <yettsyknapp@SDMacBookPro3-2.local>
2020-06-05 16:28:40 -0600

    set up rest spring boot

---
+++ JobTracker/src/main/java/com/skilldistillery/job/JobTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackerApplication.class, args);
+	}
+
+}
