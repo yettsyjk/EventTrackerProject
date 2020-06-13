0720795fbdd22c134d76d0f1b21e48570630ff92
Yettsy Knapp <yettsyknapp@SDMacBookPro3-2.local>
2020-06-13 06:04:49 -0600

    initial setup webapp/js flies 🎹

---
+++ JobTracker/src/main/webapp/js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(e){
+	console.log('you rang?');
+	init();
+});
+
+
+
+function init(){
+	document.fileForm.lookup.addEventListener('click', function(event){
+		e.prevenDefault();
+	});
+	
+}
